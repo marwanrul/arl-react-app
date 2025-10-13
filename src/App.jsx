2540452f74baa15616ee29dff854a8e466bf4fdc
marwanrul <marwanrul@gmail.com>
2025-10-13 13:31:56 +0700

    add google tag manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const pushDataLayer = (item) => {
+    window.datalayer = window.dataLayer || [];
+    window.dataLayer.push(item);
+  };
+
+  const increment = () => {
+    setCount((state) => state + 1);
+    pushDataLayer({ event: "button_click", type: "increment" });
+  };
+
+  const decrement = () => {
+    if (count === 0) return;
+    setCount((state) => state - 1);
+    pushDataLayer({ event: "button_click", type: "decrement" });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>ARL React App</h1>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+      <p>Count {count}</p>
+      <button type="button" onClick={decrement}>
+        Decrement
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
