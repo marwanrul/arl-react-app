3ec9b598a207721c8b8eb2822e06a8debb3b277d
marwanrul <marwanrul@gmail.com>
2025-10-14 09:00:48 +0700

    update buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   const pushDataLayer = (item) => {
     window.datalayer = window.dataLayer || [];
     window.dataLayer.push(item);
   };
 
-  const increment = () => {
-    setCount((state) => state + 1);
-    pushDataLayer({ event: "button_click", type: "increment" });
-  };
-
-  const decrement = () => {
-    if (count === 0) return;
-    setCount((state) => state - 1);
-    pushDataLayer({ event: "button_click", type: "decrement" });
-  };
-
   return (
-    <div>
+    <>
       <h1>ARL React App</h1>
-      <button type="button" onClick={increment}>
-        Increment
-      </button>
-      <p>Count {count}</p>
-      <button type="button" onClick={decrement}>
-        Decrement
-      </button>
-    </div>
+      <div className="wrapper">
+        <button
+          type="button"
+          onClick={() => pushDataLayer({ event: "mitra_analytic_clicked" })}
+        >
+          Clicked
+        </button>
+        <button
+          type="button"
+          onClick={() => pushDataLayer({ event: "mitra_analytic_view" })}
+        >
+          View
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            pushDataLayer({
+              event: "mitra_analytics_metric_clicked",
+              metric_name: "Performa Pasgar",
+              status_name: "Baik",
+            });
+          }}
+        >
+          Metric Clicked
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            pushDataLayer({ event: "mitra_analytics_info_clicked" });
+          }}
+        >
+          Info Clicked
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            pushDataLayer({
+              event: "mitra_analytics_info_level_clicked",
+              level_name: "Baik",
+            });
+          }}
+        >
+          Info Level Clicked
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            pushDataLayer({
+              event: "mitra_analytics_cta_clicked",
+              metric_name: "Performa Pasgar",
+              status_name: "Baik",
+              button_name: "Tambah Pasgar",
+            });
+          }}
+        >
+          CTA Clicked
+        </button>
+      </div>
+    </>
   );
 }
 
